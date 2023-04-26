b8750a80eaa07d46bd196f426e155ebca7fe5067
macosta2015 <macosta2015@my.fit.edu>
2023-04-26 07:36:02 -0400

    Proper files.
    Adding and git ignoring the correct files.

---
+++ src/App.js
@@ -0,0 +1,155 @@
+import React, { useState, useEffect } from 'react';
+import { Element, Link } from 'react-scroll';
+import './App.css';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from "@material-ui/core/Drawer";
+import Paper from '@mui/material/Paper';
+
+
+function App() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+  const [isOn, setIsOn] = useState(false);
+
+
+  //HandleClick for the responsive button
+  function handleClick() {
+    setIsOn(!isOn);
+  }
+
+  const handleDrawerClick = (event) => {
+    // Prevent the Drawer from closing when a Link inside it is clicked
+    event.stopPropagation();
+  };
+
+  //useEffect function 
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div>
+      {isSmallScreen ? (
+        <div className="App">
+          {/* Beginning of the Navbar */}
+          <nav>
+            <div className="toggle-button">
+              {/* <p>The light is {isOn ? 'on' : 'off'}</p> */}
+              <MenuIcon onClick={handleClick} style={{ fontSize: 40 }} />
+              {/* <Drawer open={isOn} onClick={handleDrawerClick}> */}
+              <Drawer
+                open={isOn}
+                onClick={handleDrawerClick}
+                PaperProps={{
+                  style: {
+                    background: '#6a8e6a',
+                    width: '300px',
+                    padding: '20px',
+                  },
+                }}
+              >
+
+                {/* Check if it is T or F */}
+                {console.log(isOn)}
+                {isOn ?
+                  <div style={{ padding: '5%' }}>
+                    <Link activeClass="active" to="section1" spy={true} smooth={true} duration={1000} offset={-70}
+                      onClick={() => setIsOn(false)}
+                      style={{
+                        display: 'block',
+                        margin: '10px 0',
+                        textDecoration: 'none',
+                        color: 'white',
+                        fontSize: '18px',
+                        fontWeight: 'bold'
+                      }}>
+                      What do we do?</Link>
+                    <Link activeClass="active" to="section2" spy={true} smooth={true} duration={1000} offset={-70}
+                      onClick={() => setIsOn(false)}
+                      style={{
+                        display: 'block',
+                        margin: '10px 0',
+                        textDecoration: 'none',
+                        color: 'white',
+                        fontSize: '18px',
+                        fontWeight: 'bold'
+                      }}>
+                      How we do it?</Link>
+                    <Link activeClass="active" to="section3" spy={true} smooth={true} duration={1000} offset={-70}
+                      onClick={() => setIsOn(false)}
+                      style={{
+                        display: 'block',
+                        margin: '10px 0',
+                        textDecoration: 'none',
+                        color: 'white',
+                        fontSize: '18px',
+                        fontWeight: 'bold'
+                      }}>Who are we?</Link>
+                  </div> : 'off'}
+              </Drawer>
+            </div>
+            <Link activeClass="active" to="section1" spy={true} smooth={true} duration={500} offset={-70}>
+              <i className="fas fa-chevron-down"></i>
+            </Link>
+          </nav >
+          {/* End of the Navbar */}
+          <div className="sections-container">
+            <Element name="section1" className="section1">
+              <h1>Who we are?</h1>
+              <p>Content for section 1 goes here</p>
+            </Element>
+            <Element name="section2" className="section2">
+              <h1>Section 2</h1>
+              <p>Content for section 2 goes here</p>
+            </Element>
+            <Element name="section3" className="section3">
+              <h1>Section 3</h1>
+              <p>Content for section 3 goes here</p>
+            </Element>
+          </div>
+        </div >
+      ) : (
+        // When it becomes a large screen
+        <div className="App">
+          <nav>
+            <div className="nav-items">
+              <Link activeClass="active" to="section1" spy={true} smooth={true} duration={500} offset={-70}>What do we do?</Link>
+              <Link activeClass="active" to="section2" spy={true} smooth={true} duration={500} offset={-70}>How we do it?</Link>
+              <Link activeClass="active" to="section3" spy={true} smooth={true} duration={500} offset={-70}>Who are we?</Link>
+            </div >
+            <Link activeClass="active" to="section1" spy={true} smooth={true} duration={500} offset={-70}>
+              <i className="fas fa-chevron-down"></i>
+            </Link>
+          </nav >
+          <div className="sections-container">
+            <Element name="section1" className="section1">
+              <Paper elevation={16} style={{ height: "50%", width: "50%", backgroundColor: '#f5f5f5', padding: '16px', borderRadius: '30px' }} >
+                <h1>What do we do?</h1>
+                <p>Content for section 1 goes here!</p>
+              </Paper>
+            </Element>
+            <Element name="section2" className="section2">
+              <h1>Section 2</h1>
+              <p>Content for section 2 goes here</p>
+            </Element>
+            <Element name="section3" className="section3">
+              <h1>Section 3</h1>
+              <p>Content for section 3 goes here</p>
+            </Element>
+          </div>
+        </div >
+      )
+      }
+    </div >
+  );
+}
+
+export default App;
+
+
