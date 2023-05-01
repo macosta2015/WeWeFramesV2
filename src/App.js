aa569c9b7776cd0c7ec582387374600a1bb51996
macosta2015 <macosta2015@my.fit.edu>
2023-04-30 20:08:46 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -157,6 +157,8 @@ function App() {
                 </Paper>
               </Fade>
             </Element>
+
+            {/* SECTION 2 */}
             <Element name="section2" className="section2" style={{ position: 'relative' }}>
               <Paper
                 elevation={16}
@@ -169,36 +171,46 @@ function App() {
                   float: 'left',
                 }}
               >
-                <Typography variant="h2" style={{ paddingLeft: '10%', paddingRight: '10%', color: '#587D71' }}>
-                  How we do it?
-                </Typography>
-                <div style={{ display: 'flex', flexDirection: 'row' }}> {/* Add flex direction column */}
-                  <Typography variant="h2" style={{ paddingLeft: '10%', paddingRight: '10%' }}>
-                    <p style={{ marginRight: '16px', width: '70%', marginBottom: '1em' }}>
+                <Grid container spacing={12}>
+
+                  <Grid item xl={12} xs={12} md={12} lg={12}>
+                    <Typography variant="h2" style={{ paddingLeft: '10%', paddingRight: '10%', color: '#587D71' }}>
+                      How we do it?
+                    </Typography>
+                  </Grid>
+                  <Grid item xl={6} xs={12} md={6} lg={6}>
+                    <Typography variant="h2" style={{ paddingLeft: '10%', paddingRight: '10%' }}>
+                      <p style={{ marginRight: '16px', width: '70%', marginBottom: '1em' }}>
+                        We use 3D printing to quickly design and build the frames.
+                        <span style={{ display: 'block', height: 'px' }}></span>
+                        3D printing is like playing with Lego blocks. You can stack different blocks on top of each other to make a cool shapes or objects.
+                        With 3D printing, instead of blocks, we use a special machine that can put together tiny bits of plastic or other materials, layer by layer, to create the shape we want.
+                      </p>
+                    </Typography>
+                  </Grid>
+                  <Grid item xl={6} xs={12} md={6} lg={6}>
+                    <img
+                      src="https://media.tenor.com/AteWDUebhk4AAAAd/3dprint-3d-printing.gif"
+                      style={{
+                        // height: '25%',
+                        // width: '25%',
+                        transition: 'transform 0.3s ease-in-out',
+                      }}
+                      onMouseEnter={(e) => e.target.style.transform = 'scale(1.2)'}
+                      onMouseLeave={(e) => e.target.style.transform = 'scale(1)'}
+                      className="responsive-image"
+                    />
+                  </Grid>
+
+                  <Grid item xl={12} xs={12} md={12} lg={12}>
+                    <p style={{ marginRight: '16px', width: '100%', marginBottom: '1em' }}>
                       We use 3D printing to quickly design and build the frames.
-                      <span style={{ display: 'block', height: 'px' }}></span>
+                      <span style={{ display: 'block', height: '20px' }}></span>
                       3D printing is like playing with Lego blocks. You can stack different blocks on top of each other to make a cool shapes or objects.
                       With 3D printing, instead of blocks, we use a special machine that can put together tiny bits of plastic or other materials, layer by layer, to create the shape we want.
                     </p>
-                  </Typography>
-                  <img
-                    src="https://media.tenor.com/AteWDUebhk4AAAAd/3dprint-3d-printing.gif"
-                    style={{
-                      height: '25%',
-                      width: '25%',
-                      transition: 'transform 0.3s ease-in-out',
-                    }}
-                    onMouseEnter={(e) => e.target.style.transform = 'scale(1.2)'}
-                    onMouseLeave={(e) => e.target.style.transform = 'scale(1)'}
-                    className="responsive-image"
-                  />
-                </div>
-                <p style={{ marginRight: '16px', width: '100%', marginBottom: '1em' }}>
-                  We use 3D printing to quickly design and build the frames.
-                  <span style={{ display: 'block', height: '20px' }}></span>
-                  3D printing is like playing with Lego blocks. You can stack different blocks on top of each other to make a cool shapes or objects.
-                  With 3D printing, instead of blocks, we use a special machine that can put together tiny bits of plastic or other materials, layer by layer, to create the shape we want.
-                </p>
+                  </Grid>
+                </Grid>
               </Paper>
             </Element>
             <Grid container direction={'row'} className="section3">
