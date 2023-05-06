aa134a2cf10aec60e27e1405f634ba4a1a2db2ed
macosta2015 <macosta2015@my.fit.edu>
2023-05-06 14:59:34 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// Let's see what happens
+// Let's see what happens, how we change the world with 3D printing
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
